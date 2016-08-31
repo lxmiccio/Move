e4d727a27fa87e43c789daca53fad5afe680bc42
Alex Miccio <miccio.alex@gmail.com>
2016-08-31 23:39:15 +0200

    Remove button is in a dangerous position

--- public/js/controllers/event.js
+++ public/js/controllers/event.js
@@ -113,19 +113,19 @@ angular.module('myControllers').controller('EventController', function($filter, 
   };
 
   vm.remove = function(event) {
-    imageService.remove({
-      image: event.image
-    }, function(response) {
-
-      eventService.remove(event.id, function(response) {
-        $window.location.href = '';
-      }, function(response) {
-        console.log(response);
-      });
-
-    }, function(response) {
-      console.log(response);
-    });
+    // imageService.remove({
+    //   image: event.image
+    // }, function(response) {
+    //
+    //   eventService.remove(event.id, function(response) {
+    //     $window.location.href = '';
+    //   }, function(response) {
+    //     console.log(response);
+    //   });
+    //
+    // }, function(response) {
+    //   console.log(response);
+    // });
   };
 
   vm.openPartecipantsPrsPdf = function(event) {
